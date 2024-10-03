9db5fe95cec7a1b170362b9b86708e9999a01552
tobibi300 <pantobibus3000@gmail.com>
2024-10-03 16:51:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ let checkbox
 document.getElementById("addButton").onclick= function(){
     task=document.getElementById("taskBox").value
     console.log(task)
-    checkbox=document.createElement("input") 
-    checkbox.type="checkbox"
-    checkbox.id="doneBox"
-    document.getElementById("taskList").appendChild(checkbox)
     paragraph=document.createElement("p")
     paragraph.textContent=task
     document.getElementById("taskList").appendChild(paragraph)
     document.getElementById("taskBox").value = ''
-
+    checkbox=document.createElement("input") 
+    checkbox.type="checkbox"
+    checkbox.id="doneBox"
+    document.getElementById("taskList").appendChild(checkbox)
+    const br=document.createElement("br")
+    br.id="brLine"
+    document.getElementById("taskList").appendChild(br)
 }
