47fc8a488c683a246b5a882d05f064b80e26c5a0
tobibi300 <pantobibus3000@gmail.com>
2024-10-13 20:29:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,70 @@
 let task
 let paragraphs = []
 let checkboxes = []
+let stevens = []
 let taskCounter = 0
+
 document.getElementById("addButton").onclick = () => {
     task = document.getElementById("taskBox").value
     console.log(task)
     taskCounter++
+
     const paragraph = document.createElement("p")
     paragraph.textContent = task
     paragraph.id = "task_" + taskCounter
+
     const checkbox = document.createElement("input")
     checkbox.type = "checkbox"
     checkbox.id = "checkBox_" + taskCounter
+
+    const steven = document.createElement("button")
+    steven.type = "button"
+    steven.id = "delete_" + taskCounter
+    steven.textContent = "x"
+
     document.getElementById("taskList").appendChild(paragraph)
     document.getElementById("taskList").appendChild(checkbox)
+    document.getElementById("taskList").appendChild(steven)
+
     const br = document.createElement("br")
     document.getElementById("taskList").appendChild(br)
     document.getElementById("taskBox").value = ''
+
+    stevens.push(steven)
     paragraphs.push(paragraph)
     checkboxes.push(checkbox)
+
     checkbox.addEventListener("change", function(event) {
         if (event.target.checked) {
             console.log("pogchamp")
+            br.remove()
             const index = checkboxes.indexOf(event.target)
-            document.getElementById("doneList").appendChild(paragraphs[index])
-            document.getElementById("doneList").appendChild(checkboxes[index])
-            const doneBr = document.createElement("br")
-            document.getElementById("doneList").appendChild(doneBr)
-
+            let wrapperDiv = document.createElement("div");
+            wrapperDiv.appendChild(paragraphs[index]);
+            wrapperDiv.appendChild(checkboxes[index]);
+            wrapperDiv.appendChild(stevens[index]);
+            document.getElementById("doneList").appendChild(wrapperDiv)
         }
         else {
             console.log("unpogchamp D:")
             const index = checkboxes.indexOf(event.target)
-            document.getElementById("taskList").appendChild(paragraphs[index])
-            document.getElementById("taskList").appendChild(checkboxes[index])
-            const doneBr = document.createElement("br")
-            document.getElementById("taskList").appendChild(doneBr)
-
+            let wrapperDiv = document.createElement("div");
+            wrapperDiv.appendChild(paragraphs[index]);
+            wrapperDiv.appendChild(checkboxes[index]);
+            wrapperDiv.appendChild(stevens[index]);
+            wrapperDiv.appendChild(stevens[index]);
+            document.getElementById("taskList").appendChild(wrapperDiv)
         }
     })
+
+    steven.onclick=(event) => {
+        console.log("stevens workin :D")
+        const stevedex = stevens.indexOf(event.target)
+        paragraphs[stevedex].remove()
+        checkboxes[stevedex].remove();
+        stevens[stevedex].remove();
+        paragraphs.splice(stevedex, 1);
+        checkboxes.splice(stevedex, 1);
+        stevens.splice(stevedex, 1);
+    }
 }
